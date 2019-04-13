7f4ed15eb294ca5fa42317f2aebca13de1299c85
Christian Jerr T. Buarao <44043066+christianjerr@users.noreply.github.com>
2019-04-13 11:32:45 +0800

    Add files via upload

---
+++ precache-manifest.37c41361398eb157b90151c7cc45eef3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "c378b65b79e3a7a1e89a2a28f6d5d14b",
+    "url": "/versett/static/media/i1.c378b65b.jpg"
+  },
+  {
+    "revision": "6b4a8fb09a8a62c3fddf",
+    "url": "/versett/static/css/main.5d6c2b1f.chunk.css"
+  },
+  {
+    "revision": "b952bc0d7d8327d62488",
+    "url": "/versett/static/js/1.b952bc0d.chunk.js"
+  },
+  {
+    "revision": "bfea850b4b2f5d299e45",
+    "url": "/versett/static/js/runtime~main.bfea850b.js"
+  },
+  {
+    "revision": "44f28ff5ddf738f8add1df097a7cb4c1",
+    "url": "/versett/static/media/arrow.44f28ff5.png"
+  },
+  {
+    "revision": "a525d567a0be46d8ad54bba201a70e7e",
+    "url": "/versett/static/media/demo.a525d567.mp4"
+  },
+  {
+    "revision": "172651a579a7f0c36b9bc33cf7a6974a",
+    "url": "/versett/static/media/c1.172651a5.jpg"
+  },
+  {
+    "revision": "6b4a8fb09a8a62c3fddf",
+    "url": "/versett/static/js/main.6b4a8fb0.chunk.js"
+  },
+  {
+    "revision": "284889a50ee495f98c1c94c33485c879",
+    "url": "/versett/static/media/i3.284889a5.jpg"
+  },
+  {
+    "revision": "925771860addcb3392927a738ca9cf6f",
+    "url": "/versett/static/media/car1.92577186.jpg"
+  },
+  {
+    "revision": "1ae82d73e8798a1382a64db39a60740b",
+    "url": "/versett/static/media/t1.1ae82d73.jpg"
+  },
+  {
+    "revision": "cfce6abbbff0099b15691345d8b94dcc",
+    "url": "/versett/static/media/gothic.cfce6abb.ttf"
+  },
+  {
+    "revision": "b6acea225b32bf935106d6329e9c77ea",
+    "url": "/versett/static/media/showcaseBg.b6acea22.jpg"
+  },
+  {
+    "revision": "f5302f9d2a5a6696b35890f6ef4bad0c",
+    "url": "/versett/index.html"
+  }
+];
